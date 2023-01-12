58389b5c05f33c74496a52bc9c4cbab686817d15
Oleh Tomashevskiy <kulemjd@ukr.net>
2023-01-12 15:15:55 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryList = document.querySelector(".gallery");
+const cardMarkup = createPictureMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", cardMarkup);
+
+function createPictureMarkup(galleryItems) {
+   
+    return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
