7a0ff93b24cc7d5c19a909670c6ca8dd9780ac01
Oleh Tomashevskiy <kulemjd@ukr.net>
2023-01-12 16:50:26 +0200

    task 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 import Player from '@vimeo/player';
+import throttle from "lodash.throttle"
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle (onPlay, 1000));
 function onPlay(event) {
     localStorage.setItem("videoplayer-current-time",event.seconds)
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+const feedbackForm = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input');
+const inputMessage = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
 
+feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
+feedbackForm.addEventListener('input', throttle(onFeedbackFormInput, 500));
+
+const dataFormUser = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+function onFeedbackFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(dataFormUser);
+}
+function onFeedbackFormInput(event) {
+    dataFormUser[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY,JSON.stringify( dataFormUser))
+    
+}
+function populateInput() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const messageParse = JSON.parse(savedMessage);
+    if (messageParse) {
+        inputEmail.value = messageParse.email || "";
+        inputMessage.value = messageParse.message || "";
+    }
+}
+populateInput()
