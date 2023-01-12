b60344b56d8a51533136d2f001033bc3f4d83e6b
Oleh Tomashevskiy <kulemjd@ukr.net>
2023-01-12 16:08:28 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
-
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', onPlay);
+function onPlay(event) {
+    localStorage.setItem("videoplayer-current-time",event.seconds)
+}
+ player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))       
